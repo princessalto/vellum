11a3c33816bcc7554c915746edcb698d5c6024a1
Princess <princessellen0016@yahoo.com>
2019-01-25 17:16:37 +0800

    Add UserAccessor to User model

--- core/submodules/User/Models/User.php
+++ core/submodules/User/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         MorphManyActivities,
         TokenizableTrait,
         TypeTrait,
+        UserAccessor,
         UserMutator,
         WhereDetailTrait,
         WhereSettingTrait;
